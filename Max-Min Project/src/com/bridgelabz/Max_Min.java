d239cdacae8967b112ce3c7ec12f3f19b8521bbc
Sankett65 <jadhav.jadhav.sanket@gmail.com>
2023-09-16 16:23:09 +0530

    Extending Max Function

--- Max-Min Project/src/com/bridgelabz/Max_Min.java
+++ Max-Min Project/src/com/bridgelabz/Max_Min.java
@@ -4,15 +4,8 @@ import java.util.Scanner;
 
 public class Max_Min {
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter First Number: ");
-        String x= sc.nextLine().toLowerCase();
-        System.out.print("Enter Second Number: ");
-        String y= sc.nextLine().toLowerCase();
-        System.out.print("Enter Third Number: ");
-        String z= sc.nextLine().toLowerCase();
 
+    public static void max(String x,String y,String z){
         if (x.length()>y.length() && x.length()>z.length()){
             System.out.println("String "+x+" is Max");
         }else if (y.length()>x.length() && y.length()>z.length()){
@@ -21,5 +14,37 @@ public class Max_Min {
             System.out.println("String "+z+" is Max");
         }
 
+    }
+
+    public static void max(String x,String y,String z,String a){
+        if (x.length()>y.length() && x.length()>z.length()){
+            System.out.println("String "+x+" is Max");
+        }else if (y.length()>x.length() && y.length()>z.length()){
+            System.out.println("String "+y+" is Max");
+        }else {
+            System.out.println("String "+z+" is Max");
+        }
+
+    }
+    public static void max(String x,String y,String z,String a,String b){
+        if (x.length()>y.length() && x.length()>z.length()){
+            System.out.println("String "+x+" is Max");
+        }else if (y.length()>x.length() && y.length()>z.length()){
+            System.out.println("String "+y+" is Max");
+        }else {
+            System.out.println("String "+z+" is Max");
+        }
+
+    }
+
+    public static void main(String[] args) {
+       String str1 = "S".toLowerCase();
+       String str2 = "Sa".toLowerCase();
+       String str3 = "San".toLowerCase();
+       String str4 = "Sak".toLowerCase();
+       String str5 = "Sae".toLowerCase();
+       String str6 = "Sat".toLowerCase();
+
+        max(str1,str2,str3);
     }
 }
